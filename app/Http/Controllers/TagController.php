9485ed1be87bf6746ba41e0ee672e144cda76d9a
Nischal <nischalshrestha800@gmail.com>
2026-03-24 17:52:04 +0545

    visibled the marketplace section

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tag;
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
@@ -21,5 +22,15 @@ class TagController extends Controller
             ->paginate(12);
 
         return view('tags.show', compact('tag', 'posts'));
+    }
+
+    public function trending()
+    {        // Trending Posts (most liked in last 7 days)
+        $trendingTags = Tag::with(['user.profile', 'tags'])
+            ->where('visibility', 'public')
+            ->where('created_at', '>=', Carbon::now()->subDays(7))->take(12)
+            ->get();
+
+        return view('tags.show', compact('trendingTags'));
     }
 }
